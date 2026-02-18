d88b8392e49de83cb0ed3abf0e2218830e4c54f7
Mridul Singh Soam <mridulssoam@gmail.com>
2026-02-18 14:51:15 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (92.67%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int min=0;
+        int max=0;
+        for (int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                min+=1;
+                max+=1;
+            }else if(s[i]==')'){
+                min--;
+                max--;
+            }else{
+                min--;
+                max++;
+            }
+            if(max<0) return false;
+            if(min<0) min = 0; 
+        }
+        return min==0;
+    }
+};
