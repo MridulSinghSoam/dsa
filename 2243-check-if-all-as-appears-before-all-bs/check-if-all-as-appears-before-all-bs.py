381d5acf7996c23632c812133439615a5f30faba
Mridul Singh Soam <mridulssoam@gmail.com>
2026-05-08 23:15:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (84.09%) - LeetSync

---
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkString(self, s: str) -> bool:
+        for i in range(1,len(s)):
+            if(s[i]=='a'):
+                if(s[i-1]=='b'):
+                    return False
+        return True
+
+
